75ece47ada5f6fe62473e0afbae84a5707e214d6
Grimdal032 <kiriell0627@gmail.com>
2023-08-04 10:34:40 +0900

    model, config

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI
+}
+
+// heroku 사이트에서 가져옴
--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const bodyParser = require("body-parser")
+
+const config = require("./config/key")
+
+const { User } = require("./models/User.js")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://ksw0627:asdf1234@cluster0.ohp872o.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true,
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send("hi!"));
+
+app.post('/register', (req, res) => {
+  // 회원가입시 필요한 정보를 client에서 가져오면
+  // 그것들을 DB에 넣어준다
+  try{
+    const user = new User(req.body);
+    user.save();
+    return res.status(200).json({ success: true });
+  }catch(err) {
+    return res.json({ success: false, err })
+  }
+})
+
 app.listen(port, () => console.log(`${port}번 포트에서 시작합니다!.`));
